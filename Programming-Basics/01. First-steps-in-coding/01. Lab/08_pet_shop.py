458b23aab12dbb001932b675070c705e053aae79
Krastin Kisov <144067293+KrissKisov@users.noreply.github.com>
2023-09-07 14:36:49 +0100

    Added files
    Added all solutions on tasks from the 'lab' in 'First steps in coding'.

---
+++ Programming-Basics/01. First-steps-in-coding/01. Lab/08_pet_shop.py
@@ -0,0 +1,10 @@
+dog_food = int(input())
+cat_food = int(input())
+price_dog_food = 2.50
+price_cat_food = 4
+total_price = dog_food * price_dog_food + cat_food * price_cat_food
+print(f"{total_price} lv.")
+# dog_food = int(input()) * 2.50
+# cat_food = int(input()) * 4
+# total_price = dog_food + cat_food
+# print(f"{total_price} lv.")
