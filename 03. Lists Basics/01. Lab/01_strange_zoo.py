e1326aa907c683a3635f04bf6e5696f3eb9509f3
KrissKisov <krisskisov@outlook.com>
2023-09-27 22:42:33 +0100

    Created folder and added a file
    Created 'Lists Basics' folder and 'Lab' subfolder, and added new file.

---
+++ 03. Lists Basics/01. Lab/01_strange_zoo.py
@@ -0,0 +1,9 @@
+# tail = input()
+# body = input()
+# head = input()
+# animal = [tail, body, head]
+# animal.reverse()
+# print(animal)
+animal = [input(), input(), input()]
+animal.reverse()
+print(animal)
