41adf40afbaa4ea532f81298e5d8fcdc4f029e4c
KrissKisov <krisskisov@outlook.com>
2023-10-09 19:02:31 +0300

    Update 03_car_race.py

--- 02. Programming-Fundamentals/03. Lists Basics/03. Lists Basics - More Exercises/03_car_race.py
+++ 02. Programming-Fundamentals/03. Lists Basics/03. Lists Basics - More Exercises/03_car_race.py
@@ -1,24 +1,47 @@
-sequence_of_numbers = input().split()
+# sequence_of_numbers = input().split()
+#
+# first_car_total_time = 0
+# second_car_total_time = 0
+#
+# for number in range(len(sequence_of_numbers) // 2):
+#     first_car_current_time = int(sequence_of_numbers[number])
+#     if first_car_current_time == 0:
+#         first_car_total_time *= 0.8
+#     first_car_total_time += first_car_current_time
+#
+# for number in range(len(sequence_of_numbers) - 1, len(sequence_of_numbers) // 2, - 1):
+#     second_car_current_time = int(sequence_of_numbers[number])
+#     if second_car_current_time == 0:
+#         second_car_total_time *= 0.8
+#     second_car_total_time += second_car_current_time
+#
+# winner = ""
+# if first_car_total_time < second_car_total_time:
+#     winner = "left"
+# elif first_car_total_time > second_car_total_time:
+#     winner = "right"
+#
+# print(f"The winner is {winner} with total time: {min(first_car_total_time, second_car_total_time) :.1f}")
 
+sequence_of_numbers = input().split()
 first_car_total_time = 0
 second_car_total_time = 0
+first_car_times = sequence_of_numbers[:len(sequence_of_numbers) // 2]
+second_car_times = sequence_of_numbers[-1:len(sequence_of_numbers) // 2: -1]
 
-for number in range(len(sequence_of_numbers) // 2):
-    first_car_current_time = int(sequence_of_numbers[number])
-    if first_car_current_time == 0:
+for time in first_car_times:
+    if int(time) == 0:
         first_car_total_time *= 0.8
-    first_car_total_time += first_car_current_time
+    first_car_total_time += int(time)
 
-for number in range(len(sequence_of_numbers) - 1, len(sequence_of_numbers) // 2, - 1):
-    second_car_current_time = int(sequence_of_numbers[number])
-    if second_car_current_time == 0:
+for time in second_car_times:
+    if int(time) == 0:
         second_car_total_time *= 0.8
-    second_car_total_time += second_car_current_time
-
-winner = ""
+    second_car_total_time += int(time)
 if first_car_total_time < second_car_total_time:
     winner = "left"
-elif first_car_total_time > second_car_total_time:
+else:
     winner = "right"
 
-print(f"The winner is {winner} with total time: {min(first_car_total_time, second_car_total_time) :.1f}")
+total_time = min(first_car_total_time, second_car_total_time)
+print(f"The winner is {winner} with total time: {total_time :.1f}")
