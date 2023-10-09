67e723ff8d5af3d7fb9587c588cc27a536612bfa
KrissKisov <krisskisov@outlook.com>
2023-10-09 23:45:35 +0300

    Update 05_tic_tac_toe.py

--- 02. Programming-Fundamentals/03. Lists Basics/03. Lists Basics - More Exercises/05_tic_tac_toe.py
+++ 02. Programming-Fundamentals/03. Lists Basics/03. Lists Basics - More Exercises/05_tic_tac_toe.py
@@ -6,41 +6,38 @@ game_field = [first_line, second_line, third_line]
 winner = ""
 
 if game_field[0][0] == "1" and game_field[0][1] == "1" and game_field[0][2] == "1":
-    winner = "first_player"
+    winner = "First player won"
 elif game_field[1][0] == "1" and game_field[1][1] == "1" and game_field[1][2] == "1":
-    winner = "first_player"
+    winner = "First player won"
 elif game_field[2][0] == "1" and game_field[2][1] == "1" and game_field[2][2] == "1":
-    winner = "first_player"
+    winner = "First player won"
 elif game_field[0][0] == "1" and game_field[1][0] == "1" and game_field[2][0] == "1":
-    winner = "first_player"
+    winner = "First player won"
 elif game_field[0][1] == "1" and game_field[1][1] == "1" and game_field[2][1] == "1":
-    winner = "first_player"
+    winner = "First player won"
 elif game_field[0][2] == "1" and game_field[1][2] == "1" and game_field[2][2] == "1":
-    winner = "first_player"
+    winner = "First player won"
 elif game_field[0][0] == "1" and game_field[1][1] == "1" and game_field[2][2] == "1":
-    winner = "first_player"
+    winner = "First player won"
 elif game_field[0][2] == "1" and game_field[1][1] == "1" and game_field[2][0] == "1":
-    winner = "first_player"
+    winner = "First player won"
 elif game_field[0][0] == "2" and game_field[0][1] == "2" and game_field[0][2] == "2":
-    winner = "second_player"
+    winner = "Second player won"
 elif game_field[1][0] == "2" and game_field[1][1] == "2" and game_field[1][2] == "2":
-    winner = "second_player"
+    winner = "Second player won"
 elif game_field[2][0] == "2" and game_field[2][1] == "2" and game_field[2][2] == "2":
-    winner = "second_player"
+    winner = "Second player won"
 elif game_field[0][0] == "2" and game_field[1][0] == "2" and game_field[2][0] == "2":
-    winner = "second_player"
+    winner = "Second player won"
 elif game_field[0][1] == "2" and game_field[1][1] == "2" and game_field[2][1] == "2":
-    winner = "second_player"
+    winner = "Second player won"
 elif game_field[0][2] == "2" and game_field[1][2] == "2" and game_field[2][2] == "2":
-    winner = "second_player"
+    winner = "Second player won"
 elif game_field[0][0] == "2" and game_field[1][1] == "2" and game_field[2][2] == "2":
-    winner = "second_player"
+    winner = "Second player won"
 elif game_field[0][2] == "2" and game_field[1][1] == "2" and game_field[2][0] == "2":
-    winner = "second_player"
-
-if winner == "first_player":
-    print("First player won")
-elif winner == "second_player":
-    print("Second player won")
+    winner = "Second player won"
 else:
-    print("Draw!")
+    winner = "Draw!"
+
+print(winner)
