fa865aff1b6b08aec6cc7a74703021c7d16dccf9
KrissKisov <krisskisov@outlook.com>
2023-10-11 17:16:58 +0300

    Create 04_repeat_string.py

---
+++ 02. Programming-Fundamentals/04. Functions/01. Functions - Lab/04_repeat_string.py
@@ -0,0 +1,14 @@
+# input_string = input()
+# counter = int(input())
+# result = lambda a,b: a * b
+# print(result(input_string, counter))
+
+def repeated_string(string, number):
+    # print(string * number)
+    return string * number
+
+
+input_string = input()
+counter = int(input())
+# repeated_string(input_string, counter)
+print(repeated_string(input_string, counter))
