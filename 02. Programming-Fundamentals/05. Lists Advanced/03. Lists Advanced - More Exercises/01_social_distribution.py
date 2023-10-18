e71273e3cbe46a7fb675fa9e9e62b71ddb126e72
KrissKisov <krisskisov@outlook.com>
2023-10-19 00:51:39 +0300

    Update 01_social_distribution.py

--- 02. Programming-Fundamentals/05. Lists Advanced/03. Lists Advanced - More Exercises/01_social_distribution.py
+++ 02. Programming-Fundamentals/05. Lists Advanced/03. Lists Advanced - More Exercises/01_social_distribution.py
@@ -1,18 +1,22 @@
+def fair_distribution(wealth_list: list, minimum: int) -> bool:
+    if sum(wealth_list) / len(wealth_list) >= minimum:
+        for number in wealth_list:
+            if number < minimum:
+                number_index = wealth_list.index(number)
+                difference = minimum - number
+                max_wealth = max(wealth_list)
+                max_wealth_index = wealth_list.index(max_wealth)
+                if max_wealth - difference >= minimum:
+                    wealth_list[max_wealth_index] -= difference
+                    wealth_list[number_index] += difference
+        return True
+    return False
+
+
 population = [int(num) for num in input().split(", ")]
 minimum_wealth = int(input())
-distributed_wealth = []
 
-if sum(population) / len(population) >= minimum_wealth:
-    for number in population:
-        if number < minimum_wealth:
-            number_index = population.index(number)
-            difference = minimum_wealth - number
-            max_wealth = max(population)
-            max_wealth_index = population.index(max_wealth)
-            if max_wealth - difference >= minimum_wealth:
-                population[max_wealth_index] -= difference
-                population[number_index] += difference
+if fair_distribution(population, minimum_wealth):
     print(population)
-
 else:
     print("No equal distribution possible")
