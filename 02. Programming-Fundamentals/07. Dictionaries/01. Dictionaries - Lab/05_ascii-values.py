e31ef4b21f338d89742a18efeb4cf00468e97864
KrissKisov <krisskisov@outlook.com>
2023-11-02 22:53:56 +0000

    Create 05_ascii-values.py

---
+++ 02. Programming-Fundamentals/07. Dictionaries/01. Dictionaries - Lab/05_ascii-values.py
@@ -0,0 +1,6 @@
+characters = input().split(', ')
+print({char: ord(char) for char in characters})
+# characters_dictionary = {}
+# for char in characters:
+#     characters_dictionary[char] = ord(char)
+# print(characters_dictionary)
