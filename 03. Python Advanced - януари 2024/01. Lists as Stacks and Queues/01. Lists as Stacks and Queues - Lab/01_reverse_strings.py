7d0a22282a6a11ffa8683f7d12affba6b61c6899
KrissKisov <krisskisov@outlook.com>
2024-01-09 22:34:58 +0000

    Create 01_reverse_strings.py
    Created new folder and added one file.

---
+++ 03. Python Advanced - януари 2024/01. Lists as Stacks and Queues/01. Lists as Stacks and Queues - Lab/01_reverse_strings.py
@@ -0,0 +1,10 @@
+given_string = list(input())
+
+while given_string:
+    print(given_string.pop(), end="")
+
+# stack = []
+# for _ in range(len(given_string)):
+#     stack.append(given_string.pop())
+#
+# print("".join(stack))
