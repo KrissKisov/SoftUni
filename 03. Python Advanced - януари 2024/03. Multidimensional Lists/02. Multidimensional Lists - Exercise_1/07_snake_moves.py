07550fe7994ad6a62d7362ceefed8c95d4da074a
KrissKisov <krisskisov@outlook.com>
2024-01-22 11:55:15 +0000

    Update 07_snake_moves.py

--- 03. Python Advanced - януари 2024/03. Multidimensional Lists/02. Multidimensional Lists - Exercise_1/07_snake_moves.py
+++ 03. Python Advanced - януари 2024/03. Multidimensional Lists/02. Multidimensional Lists - Exercise_1/07_snake_moves.py
@@ -1,24 +1,19 @@
 from collections import deque
 
 rows, columns = [int(x) for x in input().split()]
-snake = deque(input())
-
-matrix = []
+word = deque(input())
 
 for row in range(rows):
     row_data = []
     if row % 2 == 0:
         for col in range(columns):
-            letter = snake.popleft()
+            letter = word.popleft()
             row_data.append(letter)
-            snake.append(letter)
-        matrix.append(row_data)
+            word.append(letter)
+        print(*row_data, sep="")
     else:
         for col in range(columns - 1, -1, -1):
-            letter = snake.popleft()
+            letter = word.popleft()
             row_data.append(letter)
-            snake.append(letter)
-        matrix.append(row_data[::-1])
-
-for current_row in matrix:
-    print(*current_row, sep="")
+            word.append(letter)
+        print(*row_data[::-1], sep="")
