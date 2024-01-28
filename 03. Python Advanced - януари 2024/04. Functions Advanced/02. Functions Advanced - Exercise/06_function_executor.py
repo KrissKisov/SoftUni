ff46eacd8ee8245344abc6269086dfdfb3b36a5a
KrissKisov <krisskisov@outlook.com>
2024-01-28 22:10:20 +0000

    Update 06_function_executor.py

--- 03. Python Advanced - януари 2024/04. Functions Advanced/02. Functions Advanced - Exercise/06_function_executor.py
+++ 03. Python Advanced - януари 2024/04. Functions Advanced/02. Functions Advanced - Exercise/06_function_executor.py
@@ -1,5 +1,4 @@
 def func_executor(*tuples):
-
     # return "\n".join(f"{func_to_call.__name__} - {func_to_call(*arguments)}" for func_to_call, arguments in tuples)
     #
     result = ""
@@ -10,15 +9,31 @@ def func_executor(*tuples):
     return result
 
 
-def sum_numbers(num1, num2):
-    return num1 + num2
-
-
-def multiply_numbers(num1, num2):
-    return num1 * num2
-
-
-print(func_executor(
-    (sum_numbers, (1, 2)),
-    (multiply_numbers, (2, 4))
-))
+# def sum_numbers(num1, num2):
+#     return num1 + num2
+#
+#
+# def multiply_numbers(num1, num2):
+#     return num1 * num2
+#
+#
+# print(func_executor(
+#     (sum_numbers, (1, 2)),
+#     (multiply_numbers, (2, 4))
+# ))
+#
+#
+# def make_upper(*strings):
+#     result = tuple(s.upper() for s in strings)
+#     return result
+#
+#
+# def make_lower(*strings):
+#     result = tuple(s.lower() for s in strings)
+#     return result
+#
+#
+# print(func_executor(
+#     (make_upper, ("Python", "softUni")),
+#     (make_lower, ("PyThOn",)),
+# ))
