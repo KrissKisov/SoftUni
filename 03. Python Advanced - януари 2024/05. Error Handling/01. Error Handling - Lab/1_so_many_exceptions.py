a491e1abdc0aea7e5187a1e6dfc8ec81a0d1cfa2
KrissKisov <krisskisov@outlook.com>
2024-02-02 23:05:59 +0200

    Update 1_so_many_exceptions.py

--- 03. Python Advanced - януари 2024/05. Error Handling/01. Error Handling - Lab/1_so_many_exceptions.py
+++ 03. Python Advanced - януари 2024/05. Error Handling/01. Error Handling - Lab/1_so_many_exceptions.py
@@ -9,3 +9,24 @@ for i in range(len(numbers_list)):
         result /= number
 
 print(float(result))
+
+# # Example code
+#
+# numbers_list = int(input()).split(", ")
+# result = 1
+#
+# for i in range(numbers_list):
+#     number = numbers_list[i+1]
+#     if number <= 5
+#         result *= number
+#     elif 5 < number <= 10:
+#         result /= number
+#
+# print(total)
+
+# # Example input and output
+#
+# 2, 5, 10 => 1.0
+# 4, 5, 6, 1, 3 => 10.0
+# 1, 4, 5 => 20.0
+# 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 => 0.003968253968253968
