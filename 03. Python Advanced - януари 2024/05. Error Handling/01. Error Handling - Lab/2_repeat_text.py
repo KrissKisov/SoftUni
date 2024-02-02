aabd18e8ff45ed775963f59803600676fbf9f152
KrissKisov <krisskisov@outlook.com>
2024-02-02 23:27:19 +0200

    Create 2_repeat_text.py

---
+++ 03. Python Advanced - януари 2024/05. Error Handling/01. Error Handling - Lab/2_repeat_text.py
@@ -0,0 +1,17 @@
+text = input()
+
+try:
+    times = int(input())
+    print(text * times)
+except ValueError:
+    print("Variable times must be an integer")
+
+# Example input and output
+# Ex.1
+# input -> Hello
+# input -> Bye
+# output -> Variable times must be an integer
+# Ex.2
+# input -> Hello
+# input -> 2
+# output -> HelloHello
