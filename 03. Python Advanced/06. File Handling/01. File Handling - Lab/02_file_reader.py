5e71bf7384925ff6bbe55c3df6ecd4e793ab978b
KrissKisov <krisskisov@outlook.com>
2024-02-05 23:11:10 +0200

    Create 02_file_reader.py

---
+++ 03. Python Advanced/06. File Handling/01. File Handling - Lab/02_file_reader.py
@@ -0,0 +1,13 @@
+import os
+
+path = os.path.join("new_folder", "numbers.txt")
+
+file = open(path)
+total_sum = 0
+file_info = file.readlines()
+file.close()
+
+for line in file_info:
+    total_sum += int(line.strip())
+
+print(total_sum)
