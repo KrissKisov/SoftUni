1d1d1341b8ac7fdb132ef69944b2c78e5dbf92a2
KrissKisov <krisskisov@outlook.com>
2024-02-05 23:10:35 +0200

    Create 01_file_opener.py

---
+++ 03. Python Advanced/06. File Handling/01. File Handling - Lab/01_file_opener.py
@@ -0,0 +1,10 @@
+import os.path
+
+file_name = "text.txt"
+path = os.path.join("new_folder", file_name)
+
+try:
+    file = open(path)
+    print("File found")
+except FileNotFoundError:
+    print("File is not found")
