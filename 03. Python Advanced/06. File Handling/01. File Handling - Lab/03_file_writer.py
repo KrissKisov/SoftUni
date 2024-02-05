1601effd0489e971da3efa6c4594fade1ecfddac
KrissKisov <krisskisov@outlook.com>
2024-02-05 23:16:31 +0200

    Create 03_file_writer.py

---
+++ 03. Python Advanced/06. File Handling/01. File Handling - Lab/03_file_writer.py
@@ -0,0 +1,22 @@
+# import os.path
+#
+# path = os.path.join("new_folder", "my_first_file.txt")
+#
+# file = open(path, "w")
+# file.write("I just created my first file!")
+
+
+# import os
+#
+#
+# with open("new_folder/my_first_file.txt", "a") as file:  # Not a good practice for defining path
+#     file.write("I just created my first file!")
+
+
+import os
+
+
+path = os.path.join("new_folder", "my_first_file.txt")
+
+with open(path, "a") as file:
+    file.write("I just created my first file!")
