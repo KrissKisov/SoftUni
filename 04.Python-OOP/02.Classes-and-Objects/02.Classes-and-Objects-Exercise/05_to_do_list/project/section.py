40957738c1e9c4100f75494a68cd30e8e817b158
KrissKisov <krisskisov@outlook.com>
2024-02-26 11:02:43 +0000

    Created 05_to_do_list.

---
+++ 04.Python-OOP/02.Classes-and-Objects/02.Classes-and-Objects-Exercise/05_to_do_list/project/section.py
@@ -0,0 +1,49 @@
+from typing import List
+from project.task import Task
+
+
+class Section:
+
+    def __init__(self, name: str):
+        self.name = name
+        self.tasks: List = []
+
+    def add_task(self, new_task: Task) -> str:
+        if new_task in self.tasks:
+            return f"Task is already in the section {self.name}"
+
+        self.tasks.append(new_task)
+        return f"Task {Task.details(new_task)} is added to the section"
+
+    def complete_task(self, task_name: str) -> str:
+        for t in self.tasks:
+            if t.name == task_name:
+                t.completed = True
+                return f"Completed task {task_name}"
+        return f"Could not find task with the name {task_name}"
+
+    def clean_section(self) -> str:
+        removed = 0
+        for t in self.tasks:
+            if t.completed:
+                self.tasks.remove(t)
+                removed += 1
+
+        return f"Cleared {removed} tasks."
+
+    def view_section(self) -> str:
+        return f"Section {self.name}:\n" + '\n'.join(f'{Task.details(t)}' for t in self.tasks)
+
+
+task = Task("Make bed", "27/05/2020")
+print(task.change_name("Go to University"))
+print(task.change_due_date("28.05.2020"))
+task.add_comment("Don't forget laptop")
+print(task.edit_comment(0, "Don't forget laptop and notebook"))
+print(task.details())
+section = Section("Daily tasks")
+print(section.add_task(task))
+second_task = Task("Make bed", "27/05/2020")
+section.add_task(second_task)
+print(section.clean_section())
+print(section.view_section())
