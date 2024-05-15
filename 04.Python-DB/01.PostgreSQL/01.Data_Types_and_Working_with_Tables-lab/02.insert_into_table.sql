ac9ef01753976ba55738465d1ce6d6fa7c8769fb
Krastin Kisov <krisskisov@outlook.com>
2024-05-15 22:56:26 +0100

    Added new solutions.

---
+++ 04.Python-DB/01.PostgreSQL/01.Data_Types_and_Working_with_Tables-lab/02.insert_into_table.sql
@@ -0,0 +1,5 @@
+INSERT INTO employees(first_name, last_name, hiring_date, salary, devices_number)
+VALUES 
+	('Petar', 'Lolov', '2019-04-16', 3875.50, 39)
+	,('Georgi', 'Semov', '2019-10-11', 2875.00, 20)
+	,('Ivan', 'Popov', '2021-01-31', 4500.00, 80);
