b5b76d485c4fa17bf6d7b2f3a91e3589b56553a5
Krastin Kisov <krisskisov@outlook.com>
2024-05-27 14:00:55 +0100

    Create 17.elevation_peak_name.sql

---
+++ 04.Python-DB/01.PostgreSQL/06.Built-in Functions - Exercise/17.elevation_peak_name.sql
@@ -0,0 +1,15 @@
+SELECT
+	CONCAT(
+		elevation,
+		' ',
+		-- REPEAT('-', 3),
+		-- REPEAT('>', 2),
+		REPEAT('-', 3) || REPEAT('>', 2),
+		' ',
+		peak_name
+	)
+FROM
+	peaks
+WHERE
+	elevation >= 4884
+;
