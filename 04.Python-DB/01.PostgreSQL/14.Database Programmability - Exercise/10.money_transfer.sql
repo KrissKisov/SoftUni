d98e87acaac652e115d6423fe2da3a4a722d3b34
Krastin Kisov <krisskisov@outlook.com>
2024-06-13 23:57:01 +0100

    Create 10.money_transfer.sql

---
+++ 04.Python-DB/01.PostgreSQL/14.Database Programmability - Exercise/10.money_transfer.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE PROCEDURE 
+	sp_transfer_money(sender_id INT, receiver_id INT, amount NUMERIC(4))
+AS
+$$
+	DECLARE
+		sender_current_balance NUMERIC;
+	BEGIN
+		sender_current_balance := (SELECT balance FROM accounts WHERE id = sender_id);
+
+		IF sender_current_balance < amount THEN
+			ROLLBACK;
+		ELSE
+			CALL sp_withdraw_money(sender_id, amount);
+			CALL sp_deposit_money(receiver_id, amount);
+		END IF;
+	END;
+$$
+LANGUAGE plpgsql;
